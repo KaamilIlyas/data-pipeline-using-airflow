a116093f77967d1e2c5f9fb3c7acfee11b6b6cd1
Kamil Ilyas <kkamililyas@gmail.com>
2024-06-13 22:54:18 +0500

    Track articles with DVC

--- dags/scripts/store.py
+++ dags/scripts/store.py
@@ -5,21 +5,26 @@ def store_data(articles, filename):
     # Save the articles to a file
     with open(filename, 'w') as f:
         json.dump(articles, f, indent=4)
-    
+
     # Ensure that the file is tracked by DVC
     if not os.path.exists('.dvc'):
         # Only initialize DVC if it hasn't been done in this project directory
         os.system('dvc init')
-    
+
     # Add the file to DVC tracking
     os.system(f'dvc add {filename}')
     
-    # Commit changes to git (optional, if you want to keep the dvc files in git)
-    os.system(f'git add {filename}.dvc .gitignore')
-    os.system(f'git commit -m "Add/update {filename}"')
+    # Ensure the file is added to git
+    os.system(f'git add {filename}.dvc {filename} .gitignore')
 
+    # Commit changes to git
+    os.system(f'git commit -m "Add/update {filename}"')
+    
     # Push the file to the DVC remote
-    os.system(f'dvc push {filename}')
+    os.system('dvc push')
+
+    # Push git changes to the remote repository
+    os.system('git push')
 
 def main(articles):
     store_data(articles, 'articles.json')
